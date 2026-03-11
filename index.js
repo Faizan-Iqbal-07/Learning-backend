8943122aded2f9dbc016611e22949ac490e6b4ae
Faizan <faizaniqbal2911@gmail.com>
2026-03-12 04:15:39 +0500

    fixing mongodb url

--- index.js
+++ index.js
@@ -3,13 +3,12 @@ const app = express();
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 require("dotenv").config();
-const dns = require('dns');
-dns.setServers(['8.8.8.8', '1.1.1.1']);
+const dns = require("dns");
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
 const { connectDb } = require("./connection");
 const routes = require("./routes");
 
 const PORT = process.env.PORT || 4000;
-
 
 app.use(cors());
 app.use(express.json());
