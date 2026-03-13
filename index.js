b5d6089b89a568971b7be310cf8b685aede559e2
Faizan <faizaniqbal2911@gmail.com>
2026-03-13 05:51:05 +0500

    updated code

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ dns.setServers(["8.8.8.8", "1.1.1.1"]);
 const { connectDb } = require("./connection");
 const routes = require("./routes");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({ origin: ["http://localhost:5173"] , credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
